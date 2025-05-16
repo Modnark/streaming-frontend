76a58969d790ac6716eea58b7ceadf1eddfbf408
Modnark <modnarkdev@tuta.com>
2025-05-16 12:37:09 -0400

    allow one special character per name (. _ -)

--- validators/login.js
+++ validators/login.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 const loginSchema = Joi.object().keys({
-    username: Joi.string().alphanum().min(3).max(24).required(),
+    username: Joi.string().pattern(/^(?!.*[-_.].*[-_.])[a-zA-Z0-9\-_.]+$/).min(3).max(24).required(),
     password: Joi.string().min(8).max(256).required(),
 }).unknown(true);
 
